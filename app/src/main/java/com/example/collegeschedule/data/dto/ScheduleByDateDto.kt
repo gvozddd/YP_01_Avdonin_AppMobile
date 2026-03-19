41acf543ffb2b89ef4c5fba6195c5da2911405dc
gvozddd <d.avdonin54@gmail.com>
2026-03-19 13:49:04 +0700

    Фиксы

--- app/src/main/java/com/example/collegeschedule/data/dto/ScheduleByDateDto.kt
+++ app/src/main/java/com/example/collegeschedule/data/dto/ScheduleByDateDto.kt
@@ -2,4 +2,5 @@ package com.example.collegeschedule.data.dto
 data class ScheduleByDateDto(
     val lessonDate: String, // ISO: 2026-01-12
     val weekday: String,
+    val lessons: List<LessonDto>
 )
